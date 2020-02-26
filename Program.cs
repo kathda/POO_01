f7d2903a00c8b68647560a8cb10c6cb56af1852d
kathda <61172661+kathda@users.noreply.github.com>
2020-02-26 19:03:24 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaProgramacion
+{
+    class MostrarVehiculo
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo MiVehiculo = new Vehiculo();
+            MiVehiculo.SetVehiculo("Hundai", "Sonata");
+            MiVehiculo.VerVehiculo();
+            Console.WriteLine();
+
+            Vehiculo Vehiculo2 = new Vehiculo();
+            Vehiculo2.SetVehiculo("Jeep", "Grand Cherokee");
+            Vehiculo2.VerVehiculo();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaProgramacion
+{
+    class Vehiculo
+    {
+        string marca = "Honda";
+        string modelo = "Civic";
+
+        public void SetVehiculo(string marc, string model)
+        {
+            this.marca = marc;
+            this.modelo = model;
+
+        }
+        public void VerVehiculo()
+        {
+            Console.WriteLine("El vehiculo es: " +marca+ " " +modelo);
+            Console.ReadLine();
+        }
+    }
+    
+}
